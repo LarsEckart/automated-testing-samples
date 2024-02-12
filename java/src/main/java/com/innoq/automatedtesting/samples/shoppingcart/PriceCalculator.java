d74d7b0e12aba0b4e43e07474cce04e9bfc2c310
Lars Eckart <lars.eckart@hey.com>
2024-02-12 11:57:21 +0200

    same adjustments for java impl

--- java/src/main/java/com/innoq/automatedtesting/samples/shoppingcart/PriceCalculator.java
+++ java/src/main/java/com/innoq/automatedtesting/samples/shoppingcart/PriceCalculator.java
@@ -4,28 +4,62 @@ import java.math.BigDecimal;
 
 public class PriceCalculator {
     public BigDecimal calculatePrice(Article article, CustomerStatus customerStatus) {
-        if (article.name().equals("Smartphone")) {
-            return new BigDecimal("599.99");
-        } else if (article.name().equals("Coffee Maker")) {
-            return new BigDecimal("49.99");
-        } else if (article.name().equals("Running Shoes")) {
-            return new BigDecimal("89.95");
-        } else if (article.name().equals("Wireless Headphones")) {
-            return new BigDecimal("129.99");
-        } else if (article.name().equals("Laptop")) {
-            return new BigDecimal("999.99");
-        } else if (article.name().equals("Yoga Mat")) {
-            return new BigDecimal("19.99");
-        } else if (article.name().equals("Sunglasses")) {
-            return new BigDecimal("59.95");
-        } else if (article.name().equals("Backpack")) {
-            return new BigDecimal("39.99");
-        } else if (article.name().equals("Portable Speaker")) {
-            return new BigDecimal("79.99");
-        } else if (article.name().equals("Fancy Watch")) {
-            return new BigDecimal("199.99");
-        } else {
-            throw new RuntimeException("No price for article: " + article.name());
+        BigDecimal originalPrice;
+
+        switch (article.name()) {
+            case "Smartphone":
+                originalPrice = new BigDecimal("599.99");
+                break;
+            case "Coffee Maker":
+                originalPrice = new BigDecimal("49.99");
+                break;
+            case "Running Shoes":
+                originalPrice = new BigDecimal("89.95");
+                break;
+            case "Wireless Headphones":
+                originalPrice = new BigDecimal("129.99");
+                break;
+            case "Laptop":
+                originalPrice = new BigDecimal("999.99");
+                break;
+            case "Yoga Mat":
+                originalPrice = new BigDecimal("19.99");
+                break;
+            case "Sunglasses":
+                originalPrice = new BigDecimal("59.95");
+                break;
+            case "Backpack":
+                originalPrice = new BigDecimal("39.99");
+                break;
+            case "Portable Speaker":
+                originalPrice = new BigDecimal("79.99");
+                break;
+            case "Fancy Watch":
+                originalPrice = new BigDecimal("199.99");
+                break;
+            default:
+                throw new RuntimeException("No price for article: " + article.name());
         }
+
+        BigDecimal discountPercentage;
+        switch (customerStatus) {
+            case REGULAR:
+                discountPercentage = new BigDecimal("0.05"); // 5% discount
+                break;
+            case SILVER:
+                discountPercentage = new BigDecimal("0.10"); // 10% discount
+                break;
+            case GOLD:
+                discountPercentage = new BigDecimal("0.15"); // 15% discount
+                break;
+            case PLATINUM:
+                discountPercentage = new BigDecimal("0.20"); // 20% discount
+                break;
+            default:
+                discountPercentage = BigDecimal.ZERO; // No discount
+        }
+
+        BigDecimal discountAmount = originalPrice.multiply(discountPercentage);
+        return originalPrice.subtract(discountAmount);
     }
 }
--- java/src/main/java/com/innoq/automatedtesting/samples/shoppingcart/ShippingCalculator.java
+++ java/src/main/java/com/innoq/automatedtesting/samples/shoppingcart/ShippingCalculator.java
@@ -1,12 +1,16 @@
 package com.innoq.automatedtesting.samples.shoppingcart;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
 
 public class ShippingCalculator {
-    public BigDecimal calculateShipping(BigDecimal amount) {
-        if (amount.compareTo(new BigDecimal(100)) > 0) {
-            return BigDecimal.ZERO;
-        }
-        return amount.multiply(new BigDecimal("0.1"));
+    public BigDecimal calculateShipping(BigDecimal subtotalAmount) {
+        Random random = new Random();
+        BigDecimal randomNumber = BigDecimal.valueOf(random.nextDouble()).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal randomNumber2 = BigDecimal.valueOf(random.nextDouble()).setScale(2, RoundingMode.HALF_UP);
+        return randomNumber.compareTo(new BigDecimal("0.7")) < 0 ?
+                (randomNumber2.compareTo(new BigDecimal("0.3")) < 0 ? new BigDecimal("10") : new BigDecimal("5")) :
+                BigDecimal.ZERO;
     }
 }
